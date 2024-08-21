441e41283ac527b75c99823f9e2b7e0057aac08b
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-08-21 14:44:28 +0400

    create course
    # get all courses
    # get single course
    # create course

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const {body, validationResult } = require('express-validator');
+const { title } = require('process');
+
 const app = express();
+
+
+
+app.use(express.json()); // read el body eno type: json 
+// app.use(bodyParser.json()); // de gowa el express // read el body eno type: json 
 
 const courses= [
     {
@@ -22,10 +30,13 @@ const courses= [
 
 //CRUD : CREATE - READ - UPDATE - DELETE
 
+//gett all courses
 app.get('/api/courses', (req, res) => {
     res.json(courses); 
     // res.send(); (send) -> sends any type of data 
 })
+
+//get single course
 
 /* static: */
 /* app.get('/api/courses/1', (req, res) => {
@@ -34,13 +45,51 @@ app.get('/api/courses', (req, res) => {
 }) */
 
 /* dynamic: */
-app.get('/api/courses/:courseID', (req, res) => {
+app.get('/api/courses/:courseID', (req, res) => { //req.params (get parameters)
     const courseID = +req.params.courseID; ///ay 7aga btegy mn el url => type: string // "+" type: number
     const course = courses.find((course) => course.id === courseID) //finding from array courses
     if(!course){
         return res.status(404).json({msg: "course not found"});
     }
     res.json(course);
+})
+
+//create new course
+
+app.post('/api/courses/', 
+    [
+        body('title')
+            .notEmpty()
+            .withMessage("title is required")
+            .isLength({min: 2})
+            .withMessage(" min 2 char in title"),
+        body('price')
+            .notEmpty()
+            .withMessage("price is required")
+    ], (req, res) => {
+    // console.log(req.body);
+
+    const errors = validationResult(req);
+    // console.log(errors);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
+
+        //validations 
+    /* if(!req.body.title){
+        return res.status(400).json({error: ' title not provided '})
+    }
+    if(!req.body.price){
+        return res.status(400).json({error: ' price not provided '})
+    } */
+
+    const course = ({id: courses.length +1, ...req.body});
+    courses.push(course);
+
+    /* return all courses + added one */
+    // courses.push({id: courses.length +1, ...req.body});
+
+    res.status(201).json(course)
 })
 
 app.listen(4000, () =>{
