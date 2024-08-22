29620ce4a5f5316616c1a2bc708d5252238c6647
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-08-22 19:42:41 +0400

    middleware handler

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+const httpStatusText = require('./utils/httpStatusText');
+
 const url = process.env.MONGO_URL;
 
 mongoose.connect(url).then(() =>{
@@ -20,6 +22,10 @@ app.use(express.json()); // read el body eno type: json
 app.use('/api/courses', coursesRouter) //ay 7aga hategy 3al "/api/courses" haywadeeha lel courses router
 // localhost /api/courses ==> /   (from the courses router)
 
+//middle ware handler
+app.all('*', (req, res, next) => {
+    res.status(404).json({status: httpStatusText.ERROR, message: 'NOT FOUND'});
+})
 
 app.listen(process.env.PORT || 4000, () =>{
     console.log( "hello, welcome, listening on port: 4000 ");  
