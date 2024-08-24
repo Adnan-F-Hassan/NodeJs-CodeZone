ca9afa7da52fe2be5997bd9a8cd132a33ca8da0d
Adnan-F-Hassan <122805736+Adnan-F-Hassan@users.noreply.github.com>
2024-08-24 13:40:30 +0400

    password hashing

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,6 +2,7 @@ let User = require("../models/user.model");
 const httpStatusText = require('../utils/httpStatusText');
 const asyncWrapper = require("../middlewares/asyncWrapper");
 const appError = require("../utils/appError");
+const bcrypt = require('bcrypt');
 
 const getAllUsers = asyncWrapper( async (req, res) => {
     const query = req.query;
@@ -10,10 +11,9 @@ const getAllUsers = asyncWrapper( async (req, res) => {
     const page = query.page || 1;
     const skip = (page - 1 ) * limit;
 
-    const users = await User.find({}, {'__v': false}).limit(limit).skip(skip);
+    const users = await User.find({}, {'__v': false, 'password': false}).limit(limit).skip(skip);
     res.json({ status: httpStatusText.SUCCESS, data: {users}});
 }) 
-
 
 
 const register = asyncWrapper( async(req, res, next) => {
@@ -25,20 +25,18 @@ const register = asyncWrapper( async(req, res, next) => {
         return next(error);
     }
 
+    /* Hashing the password */
+    const hashedPassword = await bcrypt.hash(password, 10 )
+
     const newUser = new User({
         firstName, 
         lastName,
         email,
-        password
+        password: hashedPassword
     })
 
     await newUser.save()
     res.status(201).json({status: httpStatusText.SUCCESS,  data: {course: newUser}})
-
-
-
-
-
 }) 
 
 
